34a5dd47c5fc6657621cfcc512bf5af56e83aff1
OsiroA <osiroasunde@gmail.com>
2023-08-03 13:34:55 -0700

    17-sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,11 +8,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node->parent || !node)
 	{
-		return (0);
+		return (NULL);
 	}
 	if (node->parent->left == node)
 	{
-		return (node->parent-right);
+		return (node->parent->right);
 	}
 	return (node->parent->left);
 }
